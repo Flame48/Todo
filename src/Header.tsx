93d91760b15a3a2d5ce70e105f2a204ec0ecf57b
Flame48 <advait.s.rajan@gmail.com>
2026-03-14 22:15:37 -0500

    Fix Header Styles Again

--- src/Header.tsx
+++ src/Header.tsx
@@ -19,7 +19,7 @@ export default function Header(props: {} & PT_className) {
   return (
     <header
       className={jcn(
-        "bg-lime-800 px-6 pt-8 pb-5 sticky top-0 z-10",
+        "bg-slate-800 px-6 pt-8 pb-5 sticky top-0 z-10",
         props.className,
       )}
     >
